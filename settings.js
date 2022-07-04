2a4e5612c24372943bef189a7f75f4c6a20129ce
ABDULRAHMANIHSAN <108670362+ABDULRAHMANIHSAN@users.noreply.github.com>
2022-07-04 17:47:04 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,18 +19,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['94754680518']
+global.premium = ['754680518']
+global.ownernomer = '754680518'
+global.ownername = '🐦Abdu'
+global.botname = '🐦Abdu'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
-global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.myweb = 'https://youtube.com/channel/UCGWE4T0JzUdVJWzMAouMkmA'
+global.packname = '🐦CRAZY ᭄ABDU࿐'
+global.author = '🐦ABDU'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
